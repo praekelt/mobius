797fed838c79b20c0e8e1dc8973370a6c870d083
hedleyroos <hedleyroos@gmail.com>
2016-08-17 11:52:49 +0200

    make it able to take a local_settings

--- project/settings.py
+++ project/settings.py
@@ -99,3 +99,14 @@ CKEDITOR_UPLOAD_PATH = expanduser("~")
 
 MEDIA_ROOT = "%s/media/" % BASE_DIR
 MEDIA_URL = "/media/"
+
+try:
+    import local_settings
+    from local_settings import *
+except ImportError:
+    pass
+else:
+    if hasattr(local_settings, 'configure'):
+        lcl = locals()
+        di = local_settings.configure(**locals())
+        lcl.update(**di)
