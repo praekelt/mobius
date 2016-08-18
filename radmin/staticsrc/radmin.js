9653c6890009d0a30c4971510a4fb244373ecf49
Peter Ringelmann <peterringelmann@gmail.com>
2016-08-18 16:28:52 +0200

    react-admin: created simple react component for testing

--- radmin/staticsrc/radmin.js
+++ radmin/staticsrc/radmin.js
@@ -1,4 +1,14 @@
 /**
  * Created by peterringelmann on 2016/08/18.
  */
-console.log('loaded');
+
+import React from 'react';
+import { render } from 'react-dom';
+
+var HelloMessage = React.createClass({
+    render: function () {
+        return <h1>...and now it's even radder!</h1>;
+    }
+});
+
+render(<HelloMessage/>, document.getElementById('radmin'));
