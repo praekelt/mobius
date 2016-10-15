ede5e4bf31fbc520be211d32700cfadae1bc42b2
qoda <jpbydendyk@gmail.com>
2016-10-15 21:09:12 +0200

    Added a default home page

--- mobius/urls.py
+++ mobius/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.conf.urls import url
+from django.views import generic
+
+urlpatterns = [
+    url(
+        r"^$", generic.TemplateView.as_view(template_name="mobius/home.html"),
+        name="home"
+    ),
+]
