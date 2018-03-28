ef737df5ef26304571dd35d2024d0e23371d02ba
hedleyroos <hedleyroos@gmail.com>
2018-03-28 14:19:20 +0200

    Health check view

--- mobius/urls.py
+++ mobius/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import url
 from django.views import generic
 
+from mobius import views
+
+
 urlpatterns = [
     url(
-        r"^$", generic.TemplateView.as_view(template_name="mobius/home.html"),
+        r"^$",
+        generic.TemplateView.as_view(template_name="mobius/home.html"),
         name="home"
+    ),
+    url(
+        r"^health/$",
+        views.health,
+        name="health"
     ),
 ]
