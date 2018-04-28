a07a6b8537a2e2445b85463f46186c97f5fa0b61
hedleyroos <hedleyroos@gmail.com>
2018-04-28 11:13:39 +0200

    Allow dynamic user models

--- mobius/management/commands/create_admin_user.py
+++ mobius/management/commands/create_admin_user.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        from django.contrib.auth import get_user_model()
+        from django.contrib.auth import get_user_model
         user, created = get_user_model().objects.get_or_create(username="admin", is_superuser=1, is_staff=1)
         if created:
             user.set_password("local")
